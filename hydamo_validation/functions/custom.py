99eeeb1f4e857112de3cc781d34668ad5d085aea
Esther van der Laan <esthervdl5@gmail.com>
2026-05-01 08:23:53 +0200

    add if-else to custum functions

--- hydamo_validation/functions/custom.py
+++ hydamo_validation/functions/custom.py
@@ -296,12 +296,9 @@ def _is_linestring_straight(line) -> float:
 
     return max_cross
 
-def fix_relative_to_level(    
-    gdf,
-    datamodel,
-    parameter,
-    compare_parameter,
-    operator
+
+def fix_relative_to_level(
+    gdf, datamodel, parameter, compare_parameter, operator
 ) -> pd.Series:
     """
     Check if the value of a parameter in an object is lower/greater than an
@@ -333,16 +330,10 @@ def fix_relative_to_level(
         result = left - right
     return result
 
-def fix_equal_or_logic(    
-    gdf,
-    datamodel,
-    if_cat_1,
-	if_cat_else,
-) -> pd.Series:
-    """
-    Check if the value of a parameter in an object is lower/greater than an
-    upstream/downstream value of another parameter from another object-layer.
 
+def if_else(gdf, datamodel, logic, true, false, attribute) -> pd.Series:
+    """
+    Fill in value based on check result (TRUE/FALSE)
 
     Parameters
     ----------
@@ -350,43 +341,31 @@ def fix_equal_or_logic(
         ExtendedGeoDataFrame, HyDAMO hydroobject layer
     datamodel : HyDAMO
         HyDAMO datamodel class
+    logic: Pandas Series
+        contains results of logical test (True/False)
+    true: string, int, float
+        value when True
+    false: string, int, float
+        value when False
+    attribute: string
+        name of attribute which needs to be changed
 
     Returns
     -------
     Pandas Series
     """
-    cat = 1
 
-    if cat == 1:
-        result = if_cat_1
-    else:
-        result = if_cat_else
+    # selecteer juiste colomn (op basis van attribute) en maak daarvan pd.Series van
+    if attribute in gdf.columns:
+        raw_series = gdf[attribute]
+
+    # pas deze pd.Series aan op basis van result
+    for i in logic.index:
+        if not logic[i]:
+            raw_series[i] = false
+        else:
+            raw_series[i] = true
+
+    result = raw_series
 
     return result
-
-def if_else(    
-    gdf,
-    datamodel,
-    logic,
-    true,
-	false,
-) -> pd.Series:
-    """
-    Check if the value of a parameter in an object is lower/greater than an
-    upstream/downstream value of another parameter from another object-layer.
-
-
-    Parameters
-    ----------
-    gdf : ExtendedGeoDataframe
-        ExtendedGeoDataFrame, HyDAMO hydroobject layer
-    datamodel : HyDAMO
-        HyDAMO datamodel class
-
-    Returns
-    -------
-    Pandas Series
-    """
-
-    return logic
-    
